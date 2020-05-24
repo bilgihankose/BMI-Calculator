be6f64b092c61fbf2e191c03fa6bd58f5a65146e
Bilgihan Kose <bilgihankose@gmail.com>
2020-05-24 09:55:07 +0300

    add slider value

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -1,20 +1,24 @@
-//
-//  ViewController.swift
-//  BMI Calculator
-//
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightValueChanged(_ sender: UISlider) {
+        let height = String(format: "%.2F", sender.value)
+        heightLabel.text = "\(height) m"
+    }
+    
+    @IBAction func weightValueChanged(_ sender: UISlider) {
+           let weight = String(format: "%.0F", sender.value)
+            weightLabel.text = "\(weight) KG"
+    }
 }
 
